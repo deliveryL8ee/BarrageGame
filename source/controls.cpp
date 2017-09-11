f3b67a78b43e4bf9266557098fb5e06e68c19615
hatchbee1103 <hatchbee1103@yahoo.co.jp>
2017-09-12 02:14:39 +0900

    プレイヤー初期位置設定と移動判定の改善

--- source/controls.cpp
+++ source/controls.cpp
@@ -1,5 +1,6 @@
 // Include GLFW
 #include <GLFW/glfw3.h>
+#include <iostream>
 extern GLFWwindow* window;
 
 // Include GLM
@@ -16,7 +17,6 @@ float scaleV = 10.0f;
 
 //変換させる行列,ウィンドウの中央が初期位置
 glm::mat4 OriginalVector = glm::mat4(1.0f);
-glm::mat4 SetWinCenter = glm::translate(glm::mat4(), vec3(width/2, height/2, 0.0f));
 //拡大縮小行列
 glm::mat4 ScalingMatrix = glm::scale(OriginalVector, glm::vec3(scaleV));
 //平行移動行列
@@ -30,9 +30,8 @@ glm::mat4 getModelMatrix(){
 
 
 // Initial model position
-float Xpos = 0.0f; 
-float Ypos = 0.0f;
-float poslim = 0.0f;
+float Xpos = width/2.0f;
+float Ypos = height/2.0f;
 
 // Initial Field of View
 float initialFoV = 45.0f;
@@ -41,36 +40,31 @@ float speed = 5.0f; // 3 units / second
 
 
 void computeMatricesFromInputs(){
+
+	std::cout << "(" << Xpos << " ," << Ypos << ")" << std::endl;
+     
      // Move forward
      if (glfwGetKey(window, 'W') == GLFW_PRESS){
-	  if(Ypos <= height/2-scaleV) {
-	       Ypos += 1.0f * speed;
-	  }
-	  else Ypos = height/2-scaleV;
+	  Ypos += 1.0f * speed;
+	  if(Ypos > height-scaleV) Ypos = height-scaleV;
      }
      // Move backward
      if (glfwGetKey(window, 'S') == GLFW_PRESS){
-	  if(Ypos >= -height/2+scaleV) {
-	       Ypos -= 1.0f * speed;
-	  }
-	  else Ypos = -height/2+scaleV;
+	  Ypos -= 1.0f * speed;
+	  if(Ypos < 0.0f+scaleV) Ypos = 0.0f+scaleV;
      }
      // Strafe right
      if (glfwGetKey(window, 'D') == GLFW_PRESS){
-	  if(Xpos <= width/2-scaleV) {
-	       Xpos += 1.0f * speed;
-	  }
-	  else Xpos = width/2-scaleV;
+	  Xpos += 1.0f * speed;
+	  if(Xpos > width-scaleV) Xpos = width-scaleV;
      }
      // Strafe left
      if (glfwGetKey(window, 'A') == GLFW_PRESS){
-	  if(Xpos >= -width/2+scaleV) {
-	       Xpos -= 1.0f * speed;
-	  }
-	  else Xpos = -width/2+scaleV;
+	  Xpos -= 1.0f * speed;
+	  if(Xpos < 0.0f+scaleV) Xpos = 0.0f+scaleV;
      }
 
      TranslationMatrix = glm::translate(glm::mat4(), glm::vec3(Xpos,Ypos,1.0f));
-     ModelMatrix = TranslationMatrix * SetWinCenter * ScalingMatrix * OriginalVector;
+     ModelMatrix = TranslationMatrix * ScalingMatrix * OriginalVector;
 
 }
